85854ba41d6064217e068fc2d309b51961268d44
itouahria <touahriaidir@gmail.com>
2023-10-25 20:52:30 +0200

    test_auto

--- jenkins_vente.sql
+++ jenkins_vente.sql
@@ -3,10 +3,6 @@ CREATE  TABLE public.jenkins_vente (
 	magasin varchar(10) null,
 	chiffre_affaire int4,
 	produit varchar(10),
-<<<<<<< HEAD
-	primary key (id)
-=======
-	primary key (id)
+  primary key (id)
 
->>>>>>> 6c99687a4c937c177b8f1e259d7b85e781440838
 );
