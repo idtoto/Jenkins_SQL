cade66f50b41ac18a00c1e144ae776efa6e80b7f
itouahria <touahriaidir@gmail.com>
2023-10-25 22:10:46 +0200

    last

--- create_table_jenkins.sql
+++ create_table_jenkins.sql
@@ -1,2 +1,3 @@
 create table or replace public.jenkins_test( id int not null,
-                           libelle varchar(10))
+                           libelle varchar(10)
+                         )
